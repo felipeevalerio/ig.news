507dca2a9fc3bd553035ade41130bc348b574998
felipeevalerio <felipeevalerio@outlook.com>
2021-08-13 19:49:53 -0300

    refactor: add authentication with GitHub using next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,52 +1,28 @@
-import {auth,firebase} from "../../services/firebase"
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import {FaGithub} from "react-icons/fa";
 import {FiX} from "react-icons/fi";
 
 import styles from "./styles.module.scss";
-import { useState } from "react";
-
-interface User {
-    id:string;
-    name:string;
-}
-
 
 export function SignInButton(){
-    const [user,setUser] = useState<User>();
-
-    async function signInWithGitHub(){
-        const provider = new firebase.auth.GithubAuthProvider();
-        const result = await auth.signInWithPopup(provider); 
-        if(result.user){
-            const {displayName, uid} = result.user;
-            if(!displayName)
-                throw new Error("Missing information from GitHub Account");
-                
-            setUser({
-                name:displayName,
-                id:uid
-            })
-        }
-            
-    };
-
-    return user ? (
+    const [session] = useSession()
+    const user = session?.user
+    return session ? (
         <button 
             type="button" 
             className={styles.signInButton}
-            onClick={signInWithGitHub}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            {user.name}
+                {user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
         <button 
             type="button" 
             className={styles.signInButton} 
-            onClick={signInWithGitHub}
-
+            onClick={() => signIn("github")}
         >
             <FaGithub color="#eba417"/>
             Sign in with GitHub
