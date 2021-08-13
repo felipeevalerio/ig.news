73674703233038b1be5cd31c1ebf535a43a28911
felipeevalerio <felipeevalerio@outlook.com>
2021-08-13 18:47:30 -0300

    feat: fixed firebase enviroment keys and create authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,26 +4,41 @@ import {FaGithub} from "react-icons/fa";
 import {FiX} from "react-icons/fi";
 
 import styles from "./styles.module.scss";
+import { useState } from "react";
+
+interface User {
+    id:string;
+    name:string;
+}
 
 
 export function SignInButton(){
+    const [user,setUser] = useState<User>();
+
     async function signInWithGitHub(){
         const provider = new firebase.auth.GithubAuthProvider();
         const result = await auth.signInWithPopup(provider); 
-        if(result.user)
-            console.log(result.user);
+        if(result.user){
+            const {displayName, uid} = result.user;
+            if(!displayName)
+                throw new Error("Missing information from GitHub Account");
+                
+            setUser({
+                name:displayName,
+                id:uid
+            })
+        }
+            
     };
 
-    const isUserLoggedIn = false;
-
-    return isUserLoggedIn ? (
+    return user ? (
         <button 
             type="button" 
             className={styles.signInButton}
             onClick={signInWithGitHub}
         >
             <FaGithub color="#04d361"/>
-            Felipe Valério
+            {user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,25 +4,17 @@ import "firebase/auth";
 import "firebase/database";
 
 
-// const firebaseConfig = {
-//     apiKey:process.env.REACT_APP_API_KEY,
-//     authDomain:process.env.REACT_APP_AUTH_DOMAIN,
-//     databaseURL: process.env.REACT_APP_DATABASE_URL,
-//     projectId:process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId:process.env.REACT_APP_APP_ID 
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyCn1W4_SPb-sXriUgbmk6Zh8WUwQJmd3Jo",
-    authDomain: "ignews-8ceae.firebaseapp.com",
-    databaseURL: "https://ignews-8ceae-default-rtdb.firebaseio.com",
-    projectId: "ignews-8ceae",
-    storageBucket: "ignews-8ceae.appspot.com",
-    messagingSenderId: "883408646516",
-    appId: "1:883408646516:web:b9d639a895be79e5913c21"
-  }
+    apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId:process.env.NEXT_PUBLIC_FIREBASE_APP_ID 
+};
+
+
 
 
 if(!firebase.apps.length) 
